7b8e7eea33811b7f2bd5294f814354b90a8c0bb9
Trason Provence <trason.provence@umontana.edu>
2017-09-28 13:42:43 -0600

    Added semicolon and backslashes

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -4,6 +4,6 @@ fucntion setup() {
 // create a canvas 600px wide and 400px high
 createCanvas( 600,400 );
 
-color the background 'red'
-background( 'red' )
+// color the background 'red'
+background( 'red' );
 }
